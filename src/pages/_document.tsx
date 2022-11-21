5e0361a9cfa0489e4308bcdd8f5fe5ba66f017fb
alisonsantosofc <alisonsantoso22@gmail.com>
2022-11-21 12:07:02 -0300

    fixed favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;800&display=swap"
           rel="stylesheet"
         />
-        <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
       </Head>
 
       <body className="bg-gray-900 text-gray-200">
